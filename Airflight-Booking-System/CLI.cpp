3f502198b7892b4da2f26c9ef0ee446a618e03a4
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-11-04 12:10:33 +0200

    feat(cli): add commands

--- Airflight-Booking-System/CLI.cpp
+++ Airflight-Booking-System/CLI.cpp
@@ -1,4 +1,36 @@
+#include <iostream>
+using namespace std;
+
 class CLI {
 	void run() {
+		int choice;
+		cout << "Choose the command\n";
+		cout << "\t1. Check available places for the flight\n";
+		cout << "\t2. Buy a ticket for the flight\n";
+		cout << "\t3. Return ticket with refund\n";
+		cout << "\t4. View the booking confirmation info\n";
+		cout << "\t5. Check available places for the flight\n";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1: {
+
+		}
+		case 2: {
+
+		}
+		case 3: {
+
+		}
+		case 4: {
+
+		}
+		case 5: {
+
+		}
+		default:
+			break;
+		}
 	}
 };
